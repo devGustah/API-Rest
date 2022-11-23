a096cea7c65858b2617beef7a3ad1ae87c3f144e
Gustavo Henrique <113792269+devGustah@users.noreply.github.com>
2022-11-23 03:44:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();;
+const express = require('express');
+const app = express();
+const userRouter = require('./routes/userRouter');
+const adminRouter = require('./routes/adminRouter');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (error) => {
+        if(error)
+        console.log(error)
+        else
+        console.log("Mongo Connected")
+    })
+
+app.use('/user', express.json(), userRouter);
+
+app.get('/admin', () => {
+    res.send('Esse dado só deve ser visto pelo ADM') 
+})  
+
+app.listen(process.env.PORT, () => {
+    console.log("Server Running")
+})
